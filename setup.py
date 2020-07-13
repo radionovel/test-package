c56f6fab5d2399230996378bc9ea1e9dc949681c
Andrey Radionov <andrey.radionoff@gmail.com>
2020-07-13 07:31:44 +0500

    Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="testpackage_radionov",
-    version="0.0.2",
+    version="0.0.3",
     author="Andrey Radionov",
     author_email="andrey.radionoff@gmail.com",
     description="A small example package",
